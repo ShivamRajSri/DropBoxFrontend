ddf3534f2a53412fc93a252150289fa01632b9fc
Shivam Raj Srivastav <shivamrajsrivastav38@gmail.com>
2025-11-01 00:09:43 +0530

    Obfuscate Firebase config values
    Replaced sensitive Firebase configuration values with placeholders.

--- src/pages/DeliveryPage.tsx
+++ src/pages/DeliveryPage.tsx
@@ -36,13 +36,13 @@ import {
 // 🔥 Firebase Frontend Config
 // ===============================
 const firebaseConfig = {
-  apiKey: "AIzaSyDZqzf4-86ouuiFxS_cnzY-6o0o5qOb-5o",
-  authDomain: "react-46589.firebaseapp.com",
-  projectId: "react-46589",
-  storageBucket: "react-46589.firebasestorage.app",
-  messagingSenderId: "180036666051",
-  appId: "1:180036666051:web:65a18631204c3803995876",
-  measurementId: "G-JMTLTG9SZ4"
+  apiKey: "...",
+  authDomain: "....",
+  projectId: ".....",
+  storageBucket: ".....",
+  messagingSenderId: ".....",
+  appId: "......",
+  measurementId: "......."
 };
 
 const app = initializeApp(firebaseConfig);
